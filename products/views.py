be77a46b444e426f53e53163b176fcb70ce90da6
Divine-97 <kumwendadivine@gmail.com>
2022-04-30 15:01:50 +0000

    Added sorting Javascript and back to top link

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -26,8 +27,8 @@ def all_products(request):
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
-                    sortkey =f'-{sortkey}'
-            products = products.order_by(sortkey)        
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)      
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -43,7 +44,7 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'                  
+    current_sorting = f'{sort}_{direction}'             
 
     context = {
         'products': products,
@@ -63,4 +64,4 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/product_detail.html', context)    
+    return render(request, 'products/product_detail.html', context)
