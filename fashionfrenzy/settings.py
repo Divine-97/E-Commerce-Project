9792e4cdcb037fee6b5bc05033ebd04834eeb010
Divine-97 <kumwendadivine@gmail.com>
2023-02-07 13:13:20 +0000

    recitify debug issue in seetings.py

--- fashionfrenzy/settings.py
+++ fashionfrenzy/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v6bk2%9&&pz(^6y)a-!96m*r+z&=$)z=enw4_)bk7by6f-gr19'
+SECRET_KEY = 'os.environ.get(“SECRET_KEY”)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fashion-frenzy.herokuapp.com', 'localhost']
 
